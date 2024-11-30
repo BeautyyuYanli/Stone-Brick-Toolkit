a701ad7aad3b6652bd683f78140a3b1ef37adeaf
Yanli <mail@yanli.one>
2024-12-01 01:31:10 +0800

    enhance observability

--- src/stone_brick/observability/utils.py
+++ src/stone_brick/observability/utils.py
@@ -1,6 +1,7 @@
 from typing import Callable, TypeVar
 
 from typing_extensions import ParamSpec
+from contextlib import contextmanager
 
 try:
     from opentelemetry import trace
@@ -11,8 +12,19 @@ P = ParamSpec("P")
 T = TypeVar("T")
 
 
+def get_name(func):
+    return func.__module__ + "." + func.__name__
+
+
 def instrument(function: Callable[P, T]) -> Callable[P, T]:
     if trace is None:
         return function
     tracer = trace.get_tracer(function.__module__)
     return tracer.start_as_current_span(function.__name__)(function)  # type: ignore
+
+
+def get_span(func):
+    if trace is None:
+        return contextmanager(lambda: iter([()]))()
+    tracer = trace.get_tracer(func.__module__)
+    return tracer.start_as_current_span(get_name(func))  # type: ignore
