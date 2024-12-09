b409886273d7867c9642722b3b3ccc5c84bf946d
yanli <mail@yanli.one>
2024-12-09 16:04:01 +0800

    add bwaiter

--- src/stone_brick/asynclib/bwait.py
+++ src/stone_brick/asynclib/bwait.py
@@ -1,9 +1,11 @@
 import asyncio
 import logging
 from concurrent.futures import ThreadPoolExecutor
-from typing import Awaitable, TypeVar
+from typing import Awaitable, Callable, TypeVar
+from typing_extensions import ParamSpec
 
 T = TypeVar("T")
+P = ParamSpec("P")
 
 logger = logging.getLogger(__name__)
 
@@ -19,3 +21,13 @@ def bwait(awaitable: Awaitable[T]) -> T:
     """
     with ThreadPoolExecutor(max_workers=1) as executor:
         return executor.submit(asyncio.run, wrap_awaitable(awaitable)).result()
+
+
+P = ParamSpec("P")
+
+
+def bwaiter(func: Callable[P, Awaitable[T]]) -> Callable[P, T]:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+        return bwait(func(*args, **kwargs))
+
+    return wrapper
