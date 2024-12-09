d0bcd83ec5c4afb85a93e7710a45e57cc00a5a96
yanli <mail@yanli.one>
2024-12-09 16:20:00 +0800

    export bwaiter

--- src/stone_brick/asynclib/__init__.py
+++ src/stone_brick/asynclib/__init__.py
@@ -1,4 +1,4 @@
-from stone_brick.asynclib.bwait import bwait
+from stone_brick.asynclib.bwait import bwait, bwaiter
 from stone_brick.asynclib.cwait import CWaitable, CWaitValue, await_c
 
-__all__ = ["CWaitValue", "CWaitable", "await_c", "bwait"]
+__all__ = ["CWaitValue", "CWaitable", "await_c", "bwait", "bwaiter"]
