5790e9c5d3ce3d086bcd007342db68e55d209086
yanli <mail@yanli.one>
2025-06-18 14:18:52 +0800

    add anyio gather

--- src/stone_brick/asynclib/__init__.py
+++ src/stone_brick/asynclib/__init__.py
@@ -1,9 +1,10 @@
 from stone_brick.asynclib.bwait import bwait, bwaiter
 from stone_brick.asynclib.cwait import CWaitable, CWaitValue, await_c
-from stone_brick.asynclib.iterable_runner import (
-    stream_to_thread,
-    stream_trans_to_thread,
-)
+# from stone_brick.asynclib.iterable_runner import (
+#     stream_to_thread,
+#     stream_trans_to_thread,
+# )
+from stone_brick.asynclib.anyio_utils import gather
 
 __all__ = [
     "CWaitValue",
@@ -11,6 +12,7 @@ __all__ = [
     "await_c",
     "bwait",
     "bwaiter",
-    "stream_to_thread",
-    "stream_trans_to_thread",
+    # "stream_to_thread",
+    # "stream_trans_to_thread",
+    "gather",
 ]
