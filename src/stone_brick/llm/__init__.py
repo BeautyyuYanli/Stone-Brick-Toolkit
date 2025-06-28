352c64a344a48460e2d092e60b0aa6b470bdeae4
yanli <mail@yanli.one>
2025-06-28 17:48:25 +0800

    add event module

--- src/stone_brick/llm/__init__.py
+++ src/stone_brick/llm/__init__.py
@@ -1,4 +1,16 @@
 from stone_brick.llm.error import GeneratedEmpty, GeneratedNotValid
+from stone_brick.llm.events.events import Context, Event, EventDeps
+from stone_brick.llm.events.task_events import (
+    EndResult,
+    EventTaskOutput,
+    EventTaskOutputStream,
+    EventTaskOutputStreamDelta,
+    EventTaskStart,
+    TaskEvent,
+    TaskEventDeps,
+    TaskEventTypes,
+    print_task_event
+)
 from stone_brick.llm.utils import (
     generate_with_validation,
     oai_gen_with_retry_then_validate,
@@ -11,4 +23,17 @@ __all__ = [
     "generate_with_validation",
     "oai_gen_with_retry_then_validate",
     "oai_generate_with_retry",
+    # Events
+    "EndResult",
+    "EventTaskOutput",
+    "EventTaskOutputStream",
+    "EventTaskOutputStreamDelta",
+    "EventTaskStart",
+    "Context",
+    "Event",
+    "EventDeps",
+    "TaskEvent",
+    "TaskEventDeps",
+    "TaskEventTypes",
+    "print_task_event",
 ]
