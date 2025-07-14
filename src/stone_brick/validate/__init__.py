684d20b6cc0bccde841fe914adc096a8f2d90192
yanli <mail@yanli.one>
2025-07-14 14:06:09 +0800

    add EnvVar type for validation

--- src/stone_brick/validate/__init__.py
+++ src/stone_brick/validate/__init__.py
@@ -1,3 +1,3 @@
-from .utils import cast_type
+from .utils import EnvVar, cast_type, get_type_adapter
 
-__all__ = ["cast_type"]
+__all__ = ["EnvVar", "cast_type", "get_type_adapter"]
--- src/stone_brick/validate/utils.py
+++ src/stone_brick/validate/utils.py
@@ -1,7 +1,9 @@
+import os
+import re
 from functools import lru_cache
-from typing import Any, Type, TypeVar
+from typing import Annotated, Any, Type, TypeAlias, TypeVar
 
-from pydantic import TypeAdapter
+from pydantic import BeforeValidator, TypeAdapter
 
 T = TypeVar("T")
 
@@ -15,3 +17,21 @@ def cast_type(t: Type[T], data: Any) -> T:
     type_adapter = get_type_adapter(t)
     # type ignore due to lru_cache
     return type_adapter.validate_python(data)  # type: ignore
+
+
+def _get_str_from_env(k: str) -> str:
+    # Check if the entire string matches ${VALUE} pattern
+    pattern = r'^\$\{([^}]+)\}$'
+    match = re.match(pattern, k)
+    if match:
+        env_var_name = match.group(1)
+        v = os.getenv(env_var_name)
+        if v is None:
+            raise ValueError(f"Environment variable {env_var_name} is not set")
+        else:
+            return v
+    else:
+        raise ValueError("This field must be the pattern of ${VALUE}")
+    
+T = TypeVar("T")
+EnvVar: TypeAlias = Annotated[T, BeforeValidator(_get_str_from_env)]
